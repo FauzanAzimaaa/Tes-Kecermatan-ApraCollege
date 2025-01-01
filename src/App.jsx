92aa72efe3896e71dc2716b618c33ac828db1462
Fauzan Azima <fauzanazima606@gmail.com>
2025-01-01 14:05:39 +0800

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='font-bold text-red-600'>Project init using react + tailwind</h1>
+    </>
+  )
+}
+
+export default App
